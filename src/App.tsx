0771e84dc29d7bc5b5bd2070c958931a320b5be8
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2024-11-02 09:36:40 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+
+
+import './App.css'
+
+function App() {
+
+
+
+
+  return (
+    <>
+        <div className="parent-container">
+        <div className="main-container">
+            <h1>
+                <div>Need advice on cooking?</div>
+            </h1>
+           
+           
+            <div className="responseElement">
+             
+            
+            
+            </div>
+            <div className="inputAndButton">
+                <input className="userInput" type="text" placeholder="enter your question"/>
+                <button className="btn">click</button>
+            </div>
+       
+            <script src="index.js"></script>
+        </div>
+    </div>
+
+    </>
+  )
+}
+
+export default App
