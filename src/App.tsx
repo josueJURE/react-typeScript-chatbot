b34d1cd9dd0d1320f2da71f10cc0e5e5b8d3b5ed
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2024-11-02 09:57:28 +0000

    (feat: JS) create Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,28 @@
-
-import './App.css'
-import Header from './components/Header'
+import "./App.css";
+import Header from "./components/Header";
+import Button from "./components/Button";
 
 function App() {
-
-
-
-
   return (
     <>
-        <div className="parent-container">
+      <div className="parent-container">
         <div className="main-container">
-          <Header/>
-       
-           
-           
-            <div className="responseElement">
-             
-            
-            
-            </div>
-            <div className="inputAndButton">
-                <input className="userInput" type="text" placeholder="enter your question"/>
-                <button className="btn">click</button>
-            </div>
-       
-            <script src="index.js"></script>
-        </div>
-    </div>
+          <Header />
+          <div className="responseElement"></div>
+          <div className="inputAndButton">
+            <input
+              className="userInput"
+              type="text"
+              placeholder="enter your question"
+            />
+            <Button />
+          </div>
 
+          <script src="index.js"></script>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
